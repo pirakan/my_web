b064d3a5149a5d5bb0afd34b88aa92efd4992a2f
pirakashanth <134606891+pirakan@users.noreply.github.com>
2024-11-24 04:29:21 +0530

    Update style-switcher.js

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -39,8 +39,8 @@ dayNight.addEventListener("click",() => {
 
 window.addEventListener("load", () => {
 	if (document.body.classList.contains("dark")) {
-		dayNight.querySelector("i").classList.add("fa-sun");
-	}else{
 		dayNight.querySelector("i").classList.add("fa-moon");
+	}else{
+		dayNight.querySelector("i").classList.add("fa-sun");
 	}
 })
